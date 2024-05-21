ebc8f2c1a3185a75cb17ca58df6a327e29bdd832
Minh Bui <33414340+anonymousz97@users.noreply.github.com>
2024-05-21 17:26:30 +0700

    Update grud_model.py

--- model/grud_model.py
+++ model/grud_model.py
@@ -5,15 +5,37 @@ from .gru_model import GRUModel
 from torch.nn import functional as F
 from .activations import get_activation
 
+class CustomDiscriminator(nn.Module):
+    """
+    Custom Discriminator with custom_linear is a list of dictionary with input_dim and output_dim (e.g. [{'input_dim': 100, 'output_dim': 50}])
+    """
+    def __init__(self, custom_linear=[], activation='gelu', custom_linear_activation='gelu', device='cpu'):
+        super().__init__()
+        assert custom_linear, 'custom_linear should not be empty'
+        self.custom_linear = nn.ModuleList([nn.Linear(i['input_dim'], i['output_dim']) for i in custom_linear])
+        self.dense_prediction = nn.Linear(custom_linear[-1]['output_dim'], 1)
+        self.activation = get_activation(activation)
+        self.custom_act = get_activation(custom_linear_activation)
+    
+    def forward(self, discriminator_hidden_states):
+        for i in self.custom_linear:
+            discriminator_hidden_states = i(discriminator_hidden_states)
+            discriminator_hidden_states = self.custom_act(discriminator_hidden_states)
+        logits = torch.squeeze(self.activation(self.dense_prediction(discriminator_hidden_states)), dim=-1)
+        return logits
+
 class Discriminator(nn.Module):
     def __init__(self, hidden_size, activation='gelu'):
         super().__init__()
+        # self.dense = nn.Linear(hidden_size, 768)
+        # self.dense2 = nn.Linear(768, hidden_size)
         self.dense = nn.Linear(hidden_size, hidden_size)
         self.dense_prediction = nn.Linear(hidden_size, 1)
         self.activation = get_activation(activation)
     
     def forward(self, discriminator_hidden_states):
         hidden_states = self.dense(discriminator_hidden_states)
+        # hidden_states = self.dense2(hidden_states)
         hidden_states = self.activation(hidden_states)
         logits = torch.squeeze(self.dense_prediction(hidden_states), dim=-1)
         return logits
@@ -24,8 +46,16 @@ class GRUD(nn.Module):
         super(GRUD,self).__init__()
         self.num_classes = model_config['num_classes']
         self.generator = GRUModel(model_config, device)
-        self.discriminator = Discriminator(self.num_classes, 'sigmoid')
-
+        # self.discriminator = Discriminator(self.num_classes, 'sigmoid')
+        self.discriminator = CustomDiscriminator(
+            custom_linear = [
+                {"input_dim":self.num_classes,"output_dim":self.num_classes * 2},
+                {"input_dim":self.num_classes * 2,"output_dim":self.num_classes},
+                #{"input_dim":self.num_classes,"output_dim":self.num_classes},
+            ],
+            activation = 'sigmoid',
+            device = device
+        )
 
     def forward(self, input_):
         discriminator_logit = None
@@ -38,23 +68,6 @@ class GRUD(nn.Module):
         return logit_generator, prediction, discriminator_logit
     
 
-class CustomDiscriminator(nn.Module):
-    """
-    Custom Discriminator with custom_linear is a list of dictionary with input_dim and output_dim (e.g. [{'input_dim': 100, 'output_dim': 50}])
-    """
-    def __init__(self, custom_linear=[], activation='gelu'):
-        super().__init__()
-        assert custom_linear, 'custom_linear should not be empty'
-        self.custom_linear = []
-        for i in custom_linear:
-            self.custom_linear.append(nn.Linear(i['input_dim'], i['output_dim']))
-        self.dense_prediction = nn.Linear(custom_linear[-1]['output_dim'], 1)
-        self.activation = get_activation(activation)
-    
-    def forward(self, discriminator_hidden_states):
-        for i in self.custom_linear:
-            discriminator_hidden_states = i(discriminator_hidden_states)
-            discriminator_hidden_states = self.activation(discriminator_hidden_states)
-        logits = torch.squeeze(self.dense_prediction(discriminator_hidden_states), dim=-1)
-        return logits
+
+
     
